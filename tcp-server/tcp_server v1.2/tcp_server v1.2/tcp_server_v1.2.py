62a5775bfc0015b23b179e4f6aa0d9d246451695
qhlai <632489313@qq.com>
2019-03-25 21:49:57 +0800

    Update tcp_server_v1.2.py
    raspberry

--- tcp-server/tcp_server v1.2/tcp_server v1.2/tcp_server_v1.2.py
+++ tcp-server/tcp_server v1.2/tcp_server v1.2/tcp_server_v1.2.py
@@ -38,19 +38,44 @@ while True:
             break
 
          #  @1 #0 #0 #15.33! example
-
+        #print(data)
         if data[0:1:1] =="@":
             signal = data[0:1]
             name = data[1:3]
-            movement1 = data[4:6]
-            movement2 = data[7:9]
+            movement1 = data[4:5]
+            movement2 = data[7:8]
             angle = data[10:15]
             print(signal)
             print(name)  
             print( movement1 + movement2+angle)
-            
-        if movement1 == 1 and movement2 == 2:
-            ser.write(b"0")    #'b':表示要读写二进制数据 控制信号
+            if movement1 == "1":
+                if movement2=="0":  #高速直行无转弯
+                    ser.write(b"0")    #'b':表示要读写二进制数据 控制信号
+                elif movement2=="5": #高速右转
+                    ser.write(b"1")
+                elif movement2=="6":#高速左转
+                    ser.write(b"2")
+                elif movement2=="7":#高速弱右转
+                    ser.write(b"3")
+                elif movement2=="8":#高速弱左转
+                    ser.write(b"4")
+            elif movement1=="2":
+                if movement2=="0":  #高速倒车无转弯
+                    ser.write(b"5")    #'b':表示要读写二进制数据 控制信号
+                elif movement2=="5": #倒车强右转
+                    ser.write(b"6")
+                elif movement2=="6":#倒车强左转
+                    ser.write(b"7")
+                elif movement2=="7":#倒车弱右转
+                    ser.write(b"8")
+                elif movement2=="8":#倒车弱左转
+                    ser.write(b"9")
+
+                
+         
+
+
+
         
         
 
