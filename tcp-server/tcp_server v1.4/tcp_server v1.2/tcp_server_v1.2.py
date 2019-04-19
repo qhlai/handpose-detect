3addea3b5a3c0889d00b2ae1d5de1cb04e603a2e
qhlai <632489313@qq.com>
2019-04-20 01:16:18 +0800

    update

--- tcp-server/tcp_server v1.4/tcp_server v1.2/tcp_server_v1.2.py
+++ tcp-server/tcp_server v1.4/tcp_server v1.2/tcp_server_v1.2.py
@@ -40,26 +40,29 @@ while True:
          #  @1 #0 #0 #15.33! example
         #print(data)
         if data[0:1:1] =="@":
-            signal = data[0:1]
-            name = data[1:3]
-            movement1 = data[4:5]
-            movement2 = data[7:8]
-            ten_digit = data[10:12]
-            small_digit = data[13:15]
-            if movement1 in listdata and movement2 in listdata:
-                amovement1=movement1.encode('utf-8')
-                amovement2=movement2.encode('utf-8')
-                aten_digit=ten_digit.encode('utf-8')
-                asmall_digit=small_digit.encode('utf-8')
-                #print(signal)
-                #print(name)  
-                print( movement1 + movement2)
-                ser.write(b"{")
-                ser.write(movement1)
-                ser.write(movement2)
-                ser.write(ten_digit)
-                ser.write(small_digit)
-                ser.write(b"}")
+            #if data[::] == "0":
+                signal = data[0:1]
+                name = data[1:3]
+                movement1 = data[4:5]
+                movement2 = data[7:8]
+                ten_digit = data[10:12]
+                small_digit = data[13:15]
+                if movement1 in listdata and movement2 in listdata:
+                    amovement1=movement1.encode('utf-8')
+                    amovement2=movement2.encode('utf-8')
+                    aten_digit=ten_digit.encode('utf-8')
+                    asmall_digit=small_digit.encode('utf-8')
+                    #print(signal)
+                    #print(name)  
+                    print( movement1 + movement2)
+                    ser.write(b"{")
+                    ser.write(movement1)
+                    ser.write(movement2)
+                    
+                    ser.write(ten_digit)
+                    ser.write(small_digit)
+                    ser.write(b"}")
+            #if data[::] == "1":
 
         #send
 
